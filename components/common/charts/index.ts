739cc76c3fc90d9ac2e23580bc29e253054ad25f
Michael Manlulu <michaeldmanlulu@gmail.com>
2026-09-10 20:24:31 +0800

    chore(sveltekit): remove leftover Next.js common components
    Shared UI now lives under src/lib/components.

--- components/common/charts/index.ts
+++
@@ -1,8 +0,0 @@
-export { LineChart } from './line-chart';
-export { BarChart } from './bar-chart';
-export { PieChart } from './pie-chart';
-export { CurrencyLineChart } from './currency-line-chart';
-export { CurrencyBarChart } from './currency-bar-chart';
-export { LoanTypePieChart } from './loan-type-pie-chart';
-export { CustomTooltip } from './custom-tooltip';
-export { CashflowTrendChart } from './cashflow-trend-chart';
